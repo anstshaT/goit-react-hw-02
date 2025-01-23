136cc493ffcca7bac0b65703d65c2c0cdd9bf7df
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-01-23 22:04:25 +0100

    Add changes for step three

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Description from "./components/description/Description";
 import Options from "./components/options/Options";
 import Feedback from "./components/feedback/Feedback";
+import Notification from "./components/notification/Notification";
 
 function App() {
   const [feedback, setFeedback] = useState({
@@ -18,15 +19,21 @@ function App() {
     }));
   };
 
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
   return (
     <>
       <Description />
       <Options onOptions={updateFeedback} />
-      <Feedback
-        good={feedback.good}
-        neutral={feedback.neutral}
-        bad={feedback.bad}
-      />
+      {totalFeedback >= 1 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+        />
+      ) : (
+        <Notification />
+      )}
     </>
   );
 }
---
+++ src/components/notification/Notification.jsx
@@ -0,0 +1,7 @@
+import s from "./Notification.module.css";
+
+const Notification = () => {
+  return <div className={s.message}>No feed back yet</div>;
+};
+
+export default Notification;
